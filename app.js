4620c2314a5b609eb80641817d8185b112af819d
周立 <li.zhou@huilianyi.com>
2017-06-11 17:27:21 +0800

    Merge branch 'master' of https://github.com/QCCS/weichat-dev
    # Conflicts:
    #	app.js

--- app.js
+++ app.js
@@ -28,33 +28,19 @@ var config = {
     }
 }
 app.all('*', function (req, res) {
-    console.log(1);
 
-    console.log(22222);
-    console.log(22222);
-    console.log(22222);
-    console.log(22222);
-    console.log(22222);
-    console.log(22222);
     console.log(req.body.xml);
     //设置返回数据header
     res.writeHead(200, {'Content-Type': 'application/xml'});
     //关注后回复
     if (req.body.xml.event && req.body.xml.event === 'subscribe') {
-        var resMsg = autoReply('text', req.body.xml, '欢迎关注');
+        var resMsg = autoReply('text', req.body.xml, '欢迎关注,哈哈哈哈');
         res.end(resMsg);
     } else {
-        var info = encodeURI(req.body.xml.content);
+        console.log(req.body.xml.content);
+        var info = (req.body.xml.content);
         res.end(info);
     }
-
-
-    console.log(222223);
-    console.log(222223);
-    console.log(222223);
-    console.log(222223);
-    console.log(222223);
-
 
 });
 
